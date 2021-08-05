2a65457c87aa90118938f19433e645b7f1e94eac
meowTarae <bae94157617@gmail.com>
2021-08-06 06:33:53 +0900

    0806__06:33

---
+++ js/time.js
@@ -0,0 +1,12 @@
+const nowTime = document.querySelector(".clock__nowTime");
+
+function getNowTime() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const mins = String(date.getMinutes()).padStart(2, "0");
+    const secs = String(date.getSeconds()).padStart(2, "0");
+    nowTime.innerText = `${hours}:${mins}:${secs}`;
+}
+
+getNowTime();
+setInterval(getNowTime, 1000);

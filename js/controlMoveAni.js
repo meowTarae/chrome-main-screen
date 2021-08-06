0ea7427b2515cf381c5dce62314a5eff57652066
타래 <85001878+meowTarae@users.noreply.github.com>
2021-08-06 16:01:00 +0900

    Update controlMoveAni.js

--- js/controlMoveAni.js
+++ js/controlMoveAni.js
@@ -18,3 +18,14 @@ function ifClick() {
 }
 
 //버튼을 눌렀을 때, 버튼 애니 따로, drawer 애니 따로 작동되게끔.
+/*
+if (ifBtnClick.classList.contains('rightMoveCircle')) {
+  ifBtnClick.classList.toggle("rightMoveCircle");
+  ifBtnClick.classList.toggle("leftMoveCircle");
+} else {
+    ifBtnClick.classList.add("rightMoveCircle");
+    ifBtnClick.classList.remove("leftMoveCircle");
+}    이렇게하면 btnOnOff없어도 돌아가지않을까
+*/
+
+// 아니면? css의 애니메이션을 바로 js에서 실행할 순 없을까..?

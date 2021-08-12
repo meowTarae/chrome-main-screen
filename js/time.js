4bb2e263a0a88cb5fbd25f156ea7b5298dc1204a
meowTarae <bae94157617@gmail.com>
2021-08-13 01:09:51 +0900

    0813

--- js/time.js
+++ js/time.js
@@ -1,11 +1,23 @@
-const nowTime = document.querySelector(".nowTime");
+const nowTimeHourMinute = document.querySelector(".nowTime-HM");
+const nowTimeAMPM = document.querySelector(".nowTime-AMPM");
+const nowTimeSecond = document.querySelector(".nowTime-S");
 
 function getNowTime() {
-    const date = new Date();
-    const hours = String(date.getHours()).padStart(2, "0");
-    const mins = String(date.getMinutes()).padStart(2, "0");
-    const secs = String(date.getSeconds()).padStart(2, "0");
-    nowTime.innerText = `${hours}: ${mins}: ${secs}`;
+  const date = new Date();
+  let hours = String(date.getHours()).padStart(2, "0");
+  const mins = String(date.getMinutes()).padStart(2, "0");
+  const secs = String(date.getSeconds()).padStart(2, "0");
+
+  if (date.getHours() >= 13) {
+    hours = String(date.getHours() - 12).padStart(2, "0");
+  }
+  if (date.getHours() < 12) {
+    nowTimeAMPM.innerText = `AM`;
+  } else {
+    nowTimeAMPM.innerText = `PM`;
+  }
+  nowTimeHourMinute.innerText = `${hours}:${mins}`;
+  nowTimeSecond.innerText = `:${secs}`;
 }
 
 getNowTime();
